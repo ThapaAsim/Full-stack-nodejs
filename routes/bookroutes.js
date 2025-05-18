224db252e9093aa161892656f119683507ded8ed
ThapaAsim <thapaasim2002@gmail.com>
2025-05-18 10:03:40 +0545

    change

--- routes/bookroutes.js
+++ routes/bookroutes.js
@@ -1,7 +1,7 @@
-const { fetchbooks, addbooks, deletebooks, updatebooks } = require("../controllers/bookController")
+const { fetchbooks, addbooks, deletebooks, updatebooks, singleFetchBook } = require("../controllers/bookController")
 
 const router = require("express").Router()
 
 router.route("/book").get(fetchbooks).post(addbooks)
 
-router.route("/book/id").delete(deletebooks).patch(updatebooks)
+router.route("/book/id").delete(deletebooks).patch(updatebooks) .get(singleFetchBook)
